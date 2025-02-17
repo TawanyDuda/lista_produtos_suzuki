a7635e2ffde860be6c00b8b86588b2f081b01602
TawanyDuda <eduarda50438216@aluno.ms.senac.br>
2025-02-17 19:10:40 -0400

    primeiro commit

---
+++ view/components/sidebar.php
@@ -0,0 +1,20 @@
+<aside>
+        <button  type="button">OK</button>
+
+        <nav>
+            <ul>
+                <li>
+                    <a href="home.php">Home</a>
+                </li>
+                <li>
+                    <a href="categorias.php">Categorias</a>
+                </li>
+                <li>
+                    <a href="produtos.php">Produtos</a>
+                </li>
+                <li>
+                    <a href="usuarios.php">Usuarios</a>
+                </li>
+            </ul>
+        </nav>
+    </aside>
---
+++ view/pages/home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/treino/view/assets/css/style.css">
+</head>
+<body>
+    <?php require_once __DIR__ . '\..\components\navbar.php' ; ?>
+
+    <?php require_once __DIR__ . '\..\components\sidebar.php' ; ?>
+    
+    <main>Conteudo</main>
+
+    <?php require_once __DIR__ . '\..\components\footer.php' ; ?>
+</body>
+</html>
